014d3fe4da12f66a51e2a0250d36f292d55fbb0a
Kartik Ohri <kartikohri13@gmail.com>
2020-09-01 01:56:44 +0530

    Initial work on Wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
-#![allow(non_camel_case_types,dead_code,non_upper_case_globals)]
-#![feature(untagged_unions)]
-
 extern crate libc;
 extern crate core;
 
 #[macro_use]
-pub mod macros;
-
-#[macro_use]
-pub mod vlc;
-
-#[macro_use]
 pub mod ffi;
-
-mod types;
-mod traits;
